e2bd6c9eaff2f7adec7793e2fe7462d7daded2c3
luke.n1266@gmail.com <luke.n1266@gmail.com>
2023-05-16 17:14:02 +0700

    update 16/5

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\GiftController;
 
 /*
@@ -26,5 +27,7 @@ Route::group(['prefix' => 'admin','middleware' => ['iframeProtection','verifySho
 
 // Gift
 Route::group(['prefix' => 'api','middleware' => ['iframeProtection','verifySessionToken']], function () {
+   
     Route::post('/addGift',                [GiftController::class, 'addGift']);
+    Route::post('/sendEmail',               [ContactController::class, 'sendEmail']);
 });
